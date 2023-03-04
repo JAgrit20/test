8c508b5a92c14dfab486baf4874433e558f4d310
Jagrit Acharya <jagritacharya2020@gmail.com>
2023-03-04 17:27:00 +0530

    mc

--- test.py
+++ test.py
@@ -49,22 +49,25 @@ def Telegram_data():
                 #     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36'}
                 # response = requests.get(stock_url, headers=headers)
                 # response
+                try:
+                    curl_command = f'curl {stock_url} -H "authority: beta.nseindia.com" -H "cache-control: max-age=0" -H "dnt: 1" -H "upgrade-insecure-requests: 1" -H "user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36" -H "sec-fetch-user: ?1" -H "accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9" -H "sec-fetch-site: none" -H "sec-fetch-mode: navigate" -H "accept-encoding: gzip, deflate, br" -H "accept-language: en-US,en;q=0.9,hi;q=0.8" --compressed'
 
-                curl_command = f'curl {stock_url} -H "authority: beta.nseindia.com" -H "cache-control: max-age=0" -H "dnt: 1" -H "upgrade-insecure-requests: 1" -H "user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36" -H "sec-fetch-user: ?1" -H "accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9" -H "sec-fetch-site: none" -H "sec-fetch-mode: navigate" -H "accept-encoding: gzip, deflate, br" -H "accept-language: en-US,en;q=0.9,hi;q=0.8" --compressed'
+                    response = subprocess.check_output(curl_command, shell=True)
 
-                response = subprocess.check_output(curl_command, shell=True)
+                    data = json.loads(response.decode('utf-8'))
+                    print("dataa", data)
+                except Exception as e:
+                    print("ERROR : "+str(e))
 
-                data = json.loads(response.decode('utf-8'))
-                print("dataa", data)
-                soup = BeautifulSoup(data.text, 'html.parser')
-                data_array = soup.find(id='responseDiv').getText()
+                # soup = BeautifulSoup(data.text, 'html.parser')
+                # data_array = soup.find(id='responseDiv').getText()
 
-                y = json.loads(data_array)
+                # y = json.loads(data_array)
 
-                latest_price = (y['data'][-1]['lastPrice'])
-                latest_price = latest_price.replace(',', '')
-                print("latest", latest_price)
-                latest_price = float(latest_price)
+                # latest_price = (y['data'][-1]['lastPrice'])
+                # latest_price = latest_price.replace(',', '')
+                # print("latest", latest_price)
+                # latest_price = float(latest_price)
 
                 # name = "SUNPHARMA"
 
